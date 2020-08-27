64b5d1a5754de28a402e5d7c566b20dd942de253
Kamil Suchanek <kamilsuchanek95@gmail.com>
2020-08-27 03:42:07 +0200

    switch problem solved

--- micaff/server.R
+++ micaff/server.R
@@ -4,21 +4,21 @@ library(affy)
 library(simpleaffy)
 
 options(shiny.maxRequestSize=30000*1024^10)
-data = 0
-normali
+data <- 0
+exprs.mas5 <- 0
+exprs.rma <- 0
 
 shinyServer(function(input, output) {
-  input$
+  
   observeEvent(input$read.affymetrix.files, {
     name <- input$read.affymetrix.files$name
     datapath <- input$read.affymetrix.files$datapath
     
-    data <<- affy::read.affybatch(datapath)
+    data <- affy::read.affybatch(datapath)
   
-    normalization.algorithm <<- switch(input$normalization.algorithm,
-                                       mas5 = "mas5",
-                                       rma = "rma")
-    print(normalization.algorithm)
+    norm.alg <- switch(input$normalization.algorithm,
+                        mas5 = "mas5",
+                        rma = "rma")
+    print(norm.alg)
     })
-  
 })
