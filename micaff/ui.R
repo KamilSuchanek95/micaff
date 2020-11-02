0ea0c8d17a8e5d35c464b38930a2a6f438492617
Kamil Suchanek <kamilsuchanek95@gmail.com>
2020-11-02 18:20:13 +0100

    fixes, ui correction, draw volcano for FDR
    separation of computing statistics and making a volcano plot

--- micaff/ui.R
+++ micaff/ui.R
@@ -33,7 +33,10 @@ shinyUI(
                ),
         column(4,
                numericInput("num.genes", "Number of relevant genes", 50, min = 2, max = NA, step = 1, width = NULL),
-               actionButton("update.statistics.plots", "Update relevant genes"),
+               fluidRow(
+                 column(7,actionButton("update.statistics.plots", "Update relevant genes")),
+                 column(1,checkboxInput("FDR.number", "FDR", value = FALSE, width = NULL))
+               ),
                hr(),
                fluidRow(
                  column(10, numericInput("p.val.threshold", "Threshold for p value", 0.05, min = 0, max = 1, step = 0.0001, width = NULL)),
@@ -46,7 +49,7 @@ shinyUI(
       br(),
       fluidRow(
         column(4,actionButton("calculate.stats", "Calculate statistics and plot charts")),
-        column(3,downloadButton("downloading.normalized.data", "Download normalized data")),
+        column(4,downloadButton("downloading.normalized.data", "Download normalized data")),
         column(3,downloadButton("downloading.pvals", "Download statistics"))
       ),
       br(), hr(), br(),
